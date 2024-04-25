21561014d108816e9b494c1398d0f00ec28d457c
kunikaJangid <120725294+kunikaJangid@users.noreply.github.com>
2024-04-25 12:53:31 +0530

    register_page.java

--- register_page.java
+++ register_page.java
@@ -1,16 +1,26 @@
-package com.example.careapp; 
  
-import android.content.Intent; import android.os.Bundle; import android.text.TextUtils; import android.util.Log; import android.widget.Button; import android.widget.EditText; import android.widget.Toast; 
+import android.content.Intent; 
+import android.os.Bundle; 
+import android.text.TextUtils; 
+import android.util.Log; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.Toast; 
  
 import androidx.appcompat.app.AppCompatActivity; 
  
-import com.google.firebase.auth.FirebaseAuth; import com.google.firebase.firestore.DocumentReference; import com.google.firebase.firestore.FirebaseFirestore; 
+import com.google.firebase.auth.FirebaseAuth; 
+import com.google.firebase.firestore.DocumentReference; 
+import com.google.firebase.firestore.FirebaseFirestore; 
  
-import java.util.HashMap; import java.util.Map; import java.util.Objects; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Objects; 
  
 public class register_page extends AppCompatActivity 
 { 
-    EditText email;     EditText pass; 
+    EditText email; 
+    EditText pass; 
     EditText fname; 
     EditText phone; 
  
@@ -29,27 +39,40 @@ public class register_page extends AppCompatActivity
         
 setContentView(R.layout.activity_register_page); 
  
-        email = findViewById(R.id.email);         pass = findViewById(R.id.password);         fname = findViewById(R.id.profile_name);         phone = findViewById(R.id.phone_number);         register = findViewById(R.id.register); 
+        email = findViewById(R.id.email); 
+        pass = findViewById(R.id.password); 
+        fname = findViewById(R.id.profile_name); 
+        phone = findViewById(R.id.phone_number); 
+        register = findViewById(R.id.register); 
  
         mAuth = FirebaseAuth.getInstance(); 
         firestore = FirebaseFirestore.getInstance(); 
  
-        register.setOnClickListener(view -> createUser()); 
-    }  
+        register.setOnClickListener(view -> 
+createUser()); 
+    } 
+ 
     private void createUser() 
     { 
         String emails = email.getText().toString(); 
-        String password = pass.getText().toString();         String full_name = fname.getText().toString(); 
-        String phone_number =  phone.getText().toString(); 
+        String password = pass.getText().toString(); 
+        String full_name = fname.getText().toString(); 
+        String phone_number =  
+phone.getText().toString(); 
  
         if(TextUtils.isEmpty(emails)) 
         { 
-            email.setError("Email Cannot be Empty");             email.requestFocus(); 
+            email.setError("Email Cannot be Empty"); 
+            email.requestFocus(); 
         }else if (TextUtils.isEmpty(password)) 
         { 
-            pass.setError("Password Cannot be Empty");             pass.requestFocus(); 
-        }else             {                 
-mAuth.createUserWithEmailAndPassword(emails,password).a ddOnCompleteListener(task -> { 
+            pass.setError("Password Cannot be Empty"); 
+            pass.requestFocus(); 
+        }else 
+            { 
+                
+mAuth.createUserWithEmailAndPassword(emails,password).a
+ ddOnCompleteListener(task -> { 
                     if (task.isSuccessful()) 
                     { 
                         
@@ -58,25 +81,29 @@ successfully",Toast.LENGTH_SHORT).show();
  
                         user_id = 
 Objects.requireNonNull(mAuth.getCurrentUser()).getUid()
-; 
-                        DocumentReference documentReference = firestore.collection("User 
+ ; 
+                        DocumentReference 
+documentReference = firestore.collection("User 
 data").document(user_id); 
-                        Map<String, Object> user = new HashMap<>(); 
-                        user.put("Full Name",full_name); 
+                        Map<String, Object> user = new 
+HashMap<>(); 
+                        user.put("Full 
+Name",full_name); 
                         user.put("Email",emails); 
                         user.put("Phone 
-Number",phone_number);                         
-documentReference.set(user).addOnCompleteListener(task1 
--> Log.d("TAG","on Success: "+ user_id)); 
+Number",phone_number); 
+                        
+documentReference.set(user).addOnCompleteListener(task1 -> Log.d("TAG","on Success: "+ user_id)); 
  
                         startActivity(new 
 Intent(getApplicationContext(),MainActivity.class)); 
-                    }else                         { 
+                    }else 
+                        { 
                             
 Toast.makeText(register_page.this,"User unsuccessfully: 
 "+ 
-Objects.requireNonNull( task.getException()).getMessage (
-),Toast.LENGTH_SHORT).show(); 
+Objects.requireNonNull(task.getException()).getMessage(
+ ),Toast.LENGTH_SHORT).show(); 
                         } 
                 }); 
             } 
