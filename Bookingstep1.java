766fafbf110b29df543d78f3d982f0d20b40b7bb
kunikaJangid <120725294+kunikaJangid@users.noreply.github.com>
2024-04-25 12:57:41 +0530

    Bookingstep1.java

--- Bookingstep1.java
+++ Bookingstep1.java
@@ -13,14 +13,17 @@ import android.widget.Toast;
  
 import androidx.annotation.NonNull; 
 import androidx.annotation.Nullable; 
-import androidx.fragment.app.Fragment; import 
+import androidx.fragment.app.Fragment; 
+import 
 androidx.localbroadcastmanager.content.LocalBroadcastMa
-nager; import androidx.recyclerview.widget.GridLayoutManager; 
+nager; 
+import androidx.recyclerview.widget.GridLayoutManager; 
 import androidx.recyclerview.widget.RecyclerView; 
  
 import com.google.android.gms.tasks.OnCompleteListener; 
 import com.google.android.gms.tasks.OnFailureListener; 
-import com.google.android.gms.tasks.Task; import 
+import com.google.android.gms.tasks.Task; 
+import 
 com.google.firebase.firestore.CollectionReference; 
 import com.google.firebase.firestore.DocumentReference; 
 import com.google.firebase.firestore.DocumentSnapshot; 
@@ -30,18 +33,24 @@ com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot; 
  
 import java.text.SimpleDateFormat; 
-import java.util.ArrayList; import java.util.Calendar; 
+import java.util.ArrayList; 
+import java.util.Calendar; 
 import java.util.List; 
  
 import butterknife.BindView; 
 import butterknife.ButterKnife; 
-import butterknife.Unbinder; import 
+import butterknife.Unbinder; 
+import 
 devs.mulham.horizontalcalendar.HorizontalCalendar; 
-import devs.mulham.horizontalcalendar.HorizontalCalendarView; 
-import devs.mulham.horizontalcalendar.utils.HorizontalCalendar Listener; 
+import 
+devs.mulham.horizontalcalendar.HorizontalCalendarView; 
+import 
+devs.mulham.horizontalcalendar.utils.HorizontalCalendar
+ Listener; 
 import dmax.dialog.SpotsDialog; 
  
-public class BookingStep1Fragment extends Fragment implements ITimeSlotLoadListener { 
+public class BookingStep1Fragment extends Fragment 
+implements ITimeSlotLoadListener { 
  
     DocumentReference doctorDoc; 
     ITimeSlotLoadListener iTimeSlotLoadListener; 
@@ -54,50 +63,69 @@ public class BookingStep1Fragment extends Fragment implements ITimeSlotLoadListe
     @BindView(R.id.recycle_time_slot) 
     RecyclerView recycler_time_slot; 
     @BindView(R.id.calendarView) 
-    HorizontalCalendarView calendarView;     SimpleDateFormat simpleDateFormat; 
+    HorizontalCalendarView calendarView; 
+    SimpleDateFormat simpleDateFormat; 
  
     BroadcastReceiver displayTimeSlot = new 
-BroadcastReceiver() {         
-@Override 
-        public void onReceive(Context context, Intent intent) { 
-            Calendar date = Calendar.getInstance();            
-date.add(Calendar.DATE,0); 
+BroadcastReceiver() { 
+        @Override 
+        public void onReceive(Context context, Intent 
+intent) { 
+            Calendar date = Calendar.getInstance(); 
+            date.add(Calendar.DATE,0); 
             
-loadAvailabelTimeSlotOfDoctor(Common.CurreentDoctor,sim pleDateFormat.format(date.getTime())); 
+loadAvailabelTimeSlotOfDoctor(Common.CurreentDoctor,sim
+ pleDateFormat.format(date.getTime())); 
  
         } 
-    };  
-    private void loadAvailabelTimeSlotOfDoctor(String doctorId, final String bookDate) {         dialog.show(); 
+    }; 
+ 
+    private void loadAvailabelTimeSlotOfDoctor(String 
+doctorId, final String bookDate) { 
+        dialog.show(); 
  
         doctorDoc = FirebaseFirestore.getInstance() 
                 .collection("Doctor") 
-                .document(Common.CurreentDoctor);         doctorDoc.get().addOnCompleteListener(new 
+                .document(Common.CurreentDoctor); 
+        doctorDoc.get().addOnCompleteListener(new 
 OnCompleteListener<DocumentSnapshot>() { 
             @Override 
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) { 
+            public void onComplete(@NonNull 
+Task<DocumentSnapshot> task) { 
                 if(task.isSuccessful()) 
                 { 
-                    DocumentSnapshot documentSnapshot = task.getResult(); 
-                    if(documentSnapshot.exists()){                         CollectionReference date 
+                    DocumentSnapshot documentSnapshot = 
+task.getResult(); 
+                    if(documentSnapshot.exists()){ 
+                        CollectionReference date 
 =FirebaseFirestore.getInstance() 
                                 .collection("Doctor") 
                                 
 .document(Common.CurreentDoctor) 
                                 .collection(bookDate); 
  
-                        date.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {                            
-@Override                             public void onComplete(@NonNull Task<QuerySnapshot> task) {                                 
-if 
+                        
+date.get().addOnCompleteListener(new 
+OnCompleteListener<QuerySnapshot>() { 
+                            @Override 
+                            public void 
+onComplete(@NonNull Task<QuerySnapshot> task) { 
+                                if 
 (task.isSuccessful()) 
                                 { 
-                                    QuerySnapshot querySnapshot = task.getResult();                                     
-if 
+                                    QuerySnapshot 
+querySnapshot = task.getResult(); 
+                                    if 
 (querySnapshot.isEmpty()) 
-                                    {                                        
-iTimeSlotLoadListener.onTimeSlotLoadEmpty();                                     
-}else { 
-List<TimeSlot> timeSlots = new ArrayList<>(); 
-for (QueryDocumentSnapshot document:task.getResult())                                             
+                                    { 
+                                        
+iTimeSlotLoadListener.onTimeSlotLoadEmpty(); 
+                                    }else { 
+                                        List<TimeSlot> 
+timeSlots = new ArrayList<>(); 
+                                        for 
+(QueryDocumentSnapshot document:task.getResult()) 
+                                            
 timeSlots.add(document.toObject(TimeSlot.class)); 
                                         
 iTimeSlotLoadListener.onTimeSlotLoadSuccess(timeSlots); 
@@ -105,8 +133,131 @@ iTimeSlotLoadListener.onTimeSlotLoadSuccess(timeSlots);
 }} 
                         }).addOnFailureListener(new 
 OnFailureListener() { 
-@Override                            
-public void onFailure(@NonNull Exception e) {                                 
+                            @Override 
+                            public void 
+onFailure(@NonNull Exception e) { 
+                                
 iTimeSlotLoadListener.onTimeSlotLoadFailed(e.getMessage
-()); 
+ ()); 
+                            } 
+                        }); 
+                    } 
+                } 
+            } 
+        }); 
+    } 
  
+    static BookingStep2Fragment instance; 
+    public  static  BookingStep2Fragment getInstance(){ 
+        if(instance == null ) 
+            instance = new BookingStep2Fragment(); 
+        return instance; 
+    } 
+    @Override 
+    public void onCreate(@Nullable Bundle 
+savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+ 
+        iTimeSlotLoadListener = this; 
+ 
+        localBroadcastManager = 
+LocalBroadcastManager.getInstance(getContext()); 
+        
+localBroadcastManager.registerReceiver(displayTimeSlot,
+ new IntentFilter(Common.KEY_DISPLAY_TIME_SLOT)); 
+        simpleDateFormat = new 
+SimpleDateFormat("dd_MM_yyyy"); 
+ 
+        dialog = new 
+SpotsDialog.Builder().setContext(getContext()).setCance
+ lable(false).build(); 
+ 
+    } 
+ 
+    @Override 
+    public void onDestroy() { 
+        
+localBroadcastManager.unregisterReceiver(displayTimeSlo
+ t); 
+        super.onDestroy(); 
+    } 
+ 
+    @Nullable 
+    @Override 
+    public View onCreateView(@NonNull LayoutInflater 
+inflater, @Nullable ViewGroup container, @Nullable 
+Bundle savedInstanceState) { 
+         super.onCreateView(inflater, container, 
+savedInstanceState); 
+        View itemView = 
+inflater.inflate(R.layout.fragment_booking_step_two,con
+ tainer,false); 
+        unbinder = ButterKnife.bind(this,itemView); 
+ 
+        init(itemView); 
+        
+loadAvailabelTimeSlotOfDoctor(Common.CurreentDoctor,sim
+ pleDateFormat.format(Common.currentDate.getTime())); 
+        return itemView; 
+} 
+ 
+    private void init(View itemView) { 
+        recycler_time_slot.setHasFixedSize(true); 
+        GridLayoutManager gridLayoutManager = new 
+GridLayoutManager(getActivity(),3); 
+        
+recycler_time_slot.setLayoutManager(gridLayoutManager); 
+        //recycler_time_slot.addItemDecoration(new 
+SpaceI); 
+ 
+        Calendar startDate = Calendar.getInstance(); 
+        startDate.add(Calendar.DATE,0); 
+        Calendar endDate = Calendar.getInstance(); 
+        endDate.add(Calendar.DATE,5); 
+        HorizontalCalendar horizontalCalendar = new 
+HorizontalCalendar.Builder(itemView,R.id.calendarView) 
+                .range(startDate,endDate) 
+                .datesNumberOnScreen(1) 
+                .mode(HorizontalCalendar.Mode.DAYS) 
+                .defaultSelectedDate(startDate) 
+                .build(); 
+ 
+        horizontalCalendar.setCalendarListener(new 
+HorizontalCalendarListener() { 
+            @Override 
+            public void onDateSelected(Calendar date, 
+int position) { 
+                if(Common.currentDate.getTimeInMillis() 
+!= date.getTimeInMillis()){ 
+                    Common.currentDate = date; 
+                    
+loadAvailabelTimeSlotOfDoctor(Common.CurreentDoctor,sim
+ pleDateFormat.format(date.getTime())); 
+}}}); 
+    } 
+ 
+    @Override 
+    public void onTimeSlotLoadSuccess(List<TimeSlot> 
+timeSlotList) { 
+        MyTimeSlotAdapter adapter = new 
+MyTimeSlotAdapter(getContext(),timeSlotList); 
+        recycler_time_slot.setAdapter(adapter); 
+        dialog.dismiss(); 
+    } 
+ 
+    @Override 
+    public void onTimeSlotLoadFailed(String message) { 
+        
+Toast.makeText(getContext(),message,Toast.LENGTH_SHORT)
+ .show(); 
+        dialog.dismiss(); 
+    } 
+ 
+    @Override 
+    public void onTimeSlotLoadEmpty() { 
+        MyTimeSlotAdapter adapter = new 
+MyTimeSlotAdapter(getContext()); 
+        recycler_time_slot.setAdapter(adapter); 
+        dialog.dismiss(); 
+    } 
+} 
